dc98e9114a1a29275dada962047c94950e17269c
ROHITMAGDUM2 <rohitmagdum2@gmail.com>
2023-11-07 19:11:40 +0530

    Spring IOc using xml file Co-authored-by: Rohit Magddum <rohitmagdum2@gmail.com>

--- src/com/rohit/beans/Student.java
+++ src/com/rohit/beans/Student.java
@@ -3,27 +3,32 @@ package com.rohit.beans;
 public class Student {
 
 	private String name;
-	private int rollno ;
+	private int rollno;
 	private String email;
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getRollno() {
 		return rollno;
 	}
+
 	public void setRollno(int rollno) {
 		this.rollno = rollno;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
-	public void display () {
-		System.out.println("Name :"+name);
-		System.out.println("Rollno :"+rollno);
-		System.out.println("Email :"+email);
+
+	public void display() {
+		System.out.println("Name :" + name);
+		System.out.println("Rollno :" + rollno);
+		System.out.println("Email :" + email);
 	}
 }
